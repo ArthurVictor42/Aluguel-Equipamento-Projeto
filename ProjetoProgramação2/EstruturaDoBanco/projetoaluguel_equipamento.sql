179a975f0d0e43a392b66e35ed7c3861e03da9c1
ArthurVictor42 <arthurv.santos@unifacol.edu.br>
2025-05-20 10:52:18 -0300

    estrutura corrigida

--- ProjetoProgramação2/EstruturaDoBanco/projetoaluguel_equipamento.sql
+++ ProjetoProgramação2/EstruturaDoBanco/projetoaluguel_equipamento.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `equipamento`
 --
 
-DROP TABLE IF EXISTS `equipamento`;
+-- DROP TABLE IF EXISTS `equipamento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `equipamento` (
